c47420fe84665827f1549f14955748239451e955
LinFhl <linfhl1@gmail.com>
2020-11-05 10:15:14 +0100

    Erste Ertellung von Pokemon Attack / AttackList. Aktuell unvollständig, Erik und Linda arbeiten weiter daran.

--- src/main/java/ApplicationStart.java
+++ src/main/java/ApplicationStart.java
@@ -1,5 +1,17 @@
+import Classes.AttackList;
+import Classes.Pokemon;
+
 public class ApplicationStart {
     public static void main(String[] args) {
+
         System.out.println("This should work");
+
+        AttackList al2 = new AttackList();
+
+        System.out.println(al2.giveAttack(0).getName());
+
+
     }
+
+
 }
