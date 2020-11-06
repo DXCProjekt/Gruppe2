196ab9c08ab0f1ecf16f9a317952f71e2462b0c3
Max <maximilia.r11@schule.koeln>
2020-11-06 10:17:07 +0100

    Funktionalität der Button und Beispieltexte im Terminal. Hinzufügen aller Komponenten der battle Scene.

--- src/main/java/view/BattleController.java
+++ src/main/java/view/BattleController.java
@@ -2,14 +2,37 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 
 public class BattleController {
 
     @FXML
-    Button btnFight;
+    Button btnFight, btnBag, btnPokemon, btnRun;
+    Label tvFightText, tvPokemonLeft, tvPokemonRight;
+    ImageView ivPokemonFront, ivPokemonBack;
+    ProgressBar barLeft, barRight;
 
-    public void onButtonClick() {
-        System.out.println("Button clicked!");
+    public void btnFightClicked() {
+        System.out.println("btnFightClicked!");
+        tvSetText();
+    }
+
+    public void btnBagClicked() {
+        System.out.println("btnBagClicked!");
+    }
+
+    public void btnPokemonClicked() {
+        System.out.println("btnPokemonClicked!");
+    }
+
+    public void btnRunClicked() {
+        System.out.println("btnRunClicked!");
+    }
+
+    public void tvSetText() {
+        tvFightText.setText("Test");
     }
 
 }
