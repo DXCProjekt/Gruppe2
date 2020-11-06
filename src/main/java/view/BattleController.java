54d11e251e61d2003f390736b67154a99d34587c
Max <maximilia.r11@schule.koeln>
2020-11-06 11:24:44 +0100

    Änderungen am Resource Path, die Bilder sind nun im Projekt integriert

--- src/main/java/view/BattleController.java
+++ src/main/java/view/BattleController.java
@@ -10,29 +10,41 @@ public class BattleController {
 
     @FXML
     Button btnFight, btnBag, btnPokemon, btnRun;
+    @FXML
     Label tvFightText, tvPokemonLeft, tvPokemonRight;
+    @FXML
     ImageView ivPokemonFront, ivPokemonBack;
+    @FXML
     ProgressBar barLeft, barRight;
 
     public void btnFightClicked() {
         System.out.println("btnFightClicked!");
-        tvSetText();
+        tvFightText.setText("btnFightClicked!");
+        tvPokemonLeft.setText("btnFightClicked!");
+        tvPokemonRight.setText("btnFightClicked!");
     }
 
     public void btnBagClicked() {
         System.out.println("btnBagClicked!");
+        tvFightText.setText("btnBagClicked!");
+        tvPokemonLeft.setText("btnBagClicked!");
+        tvPokemonRight.setText("btnBagClicked!");
     }
 
     public void btnPokemonClicked() {
         System.out.println("btnPokemonClicked!");
+        tvFightText.setText("btnPokemonClicked!");
+        tvPokemonLeft.setText("btnPokemonClicked!");
+        tvPokemonRight.setText("btnPokemonClicked!");
     }
 
     public void btnRunClicked() {
         System.out.println("btnRunClicked!");
+        tvFightText.setText("btnRunClicked!");
+        tvPokemonLeft.setText("btnRunClicked!");
+        tvPokemonRight.setText("btnRunClicked!");
     }
 
-    public void tvSetText() {
-        tvFightText.setText("Test");
-    }
+
 
 }
