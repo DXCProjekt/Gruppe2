94d303fceecd833d357fa20e13c26524bb861ff5
Maximilian Ried <maximilia.r11@schule.koeln>
2020-11-09 11:15:40 +0100

    Fehlende Classen hinzugefügt (Attack und Pokemon)

---
+++ src/main/java/edu/dxc/pokemon/Attack.java
@@ -0,0 +1,57 @@
+package edu.dxc.pokemon;
+
+public class Attack {
+
+    private String name;
+    private int atk;
+    private double acc;
+    private String typ;
+
+    public Attack(String name, int atk, double acc, String typ){
+
+        this.name = name;
+        this.atk = atk;
+        this.acc = acc;
+        this.typ = typ;
+
+    }
+
+    //Test
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public double getAcc() {
+        return acc;
+    }
+
+    public void setAcc(double acc) {
+        this.acc = acc;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+
+
+
+}
