d74f17fd484a56e401a284b86252ad2c549f89b8
Maximilian Ried <maximilia.r11@schule.koeln>
2020-11-09 14:12:55 +0100

    Änderungen an dem "X Button" für die Fight GUI

--- src/main/java/edu/dxc/pokemon/GuiStart.java
+++ src/main/java/edu/dxc/pokemon/GuiStart.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import view.BattleController;
 
@@ -16,7 +17,8 @@ public class GuiStart extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../../../view/battleScene.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Pokémon");
+        // primaryStage.getIcons().add(new Image("file:pokeball.png"));
         primaryStage.setScene(new Scene(root, 720, 370));
         primaryStage.show();
     }
